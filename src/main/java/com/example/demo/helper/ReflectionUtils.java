83b8ac38be7d34e02bc45ee92391b918091178d9
xuliduo <xuliduo@126.com>
2019-07-04 12:30:25 +0800

    Upgrade com.fasterxml.jackson.core:jackson-databind to version 2.9.9

--- src/main/java/com/example/demo/helper/ReflectionUtils.java
+++ src/main/java/com/example/demo/helper/ReflectionUtils.java
@@ -6,16 +6,12 @@ import org.apache.commons.lang3.reflect.FieldUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
